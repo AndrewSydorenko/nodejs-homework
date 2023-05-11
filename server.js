6d83ea8c316a614753dd981201982888390273c4
Sam <98640251+AndrewSydorenko@users.noreply.github.com>
2023-05-11 14:45:06 +0300

    fc

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+const mongoose = require('mongoose');
+const DB_HOST = 'mongodb+srv://samtaktreba:DazVr9tYn2DwySV0@cluster0.dhtizyx.mongodb.net/db-contacts?retryWrites=true&w=majority';
+mongoose.set('strictQuery', true);
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000)
+  }).catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
 const app = require('./app')
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
