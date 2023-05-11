e9e4896e9c167a5767db9e66b1294ec175885978
Sam <98640251+AndrewSydorenko@users.noreply.github.com>
2023-05-11 22:21:06 +0300

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const ctrl = require("../../controlers/contactsControler");
-const { validateBody } = require("../../middlewares");
-const schemas = require("../../schemas/contactsSchema");
+const { validateBody, isValidId,
+    validateFavorite } = require("../../middlewares");
+const { schemas } = require("../../models/contact");
 
 const router = express.Router();
 
@@ -14,5 +15,13 @@ router.post("/", validateBody(schemas.addSchema), ctrl.add);
 router.delete("/:contactId", ctrl.deleteById);
 
 router.put("/:contactId", validateBody(schemas.addSchema), ctrl.updateById);
+
+router.patch(
+    "/:contactId/favorite",
+    isValidId,
+    validateFavorite(schemas.updateFavoriteSchema),
+    ctrl.updateFavorite
+);
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
-const DB_HOST = 'mongodb+srv://samtaktreba:DazVr9tYn2DwySV0@cluster0.dhtizyx.mongodb.net/db-contacts?retryWrites=true&w=majority';
+
+const app = require('./app')
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
 mongoose.set('strictQuery', true);
+
 mongoose.connect(DB_HOST)
   .then(() => {
-    app.listen(3000)
+    console.log("Database connection successful");
+    app.listen(PORT)
   }).catch(error => {
     console.log(error.message);
     process.exit(1);
   })
-const app = require('./app')
+
