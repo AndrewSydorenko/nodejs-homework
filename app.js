fb196fab12bac8843c1a6bb271262df0f062ba19
Sam <98640251+AndrewSydorenko@users.noreply.github.com>
2023-05-22 23:31:11 +0300

    hotFix

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
+app.use((req, res) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Invalid or missing token' })
+  }
+});
+
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
