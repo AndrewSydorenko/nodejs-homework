08359c8dd626bbedc2fdf34b83c546de48e42c0f
Sam <98640251+AndrewSydorenko@users.noreply.github.com>
2023-06-03 20:37:55 +0300

    1231

--- controlers/auth.js
+++ controlers/auth.js
@@ -87,6 +87,10 @@ const login = async (req, res) => {
         throw HttpError(401, "Email or password is wrong");
     }
 
+    if (!user.verify) {
+        throw HttpError(401, "Email not verified");
+    }
+
     const passwordCompare = await bcrypt.compare(password, user.password);
 
     if (!passwordCompare) {
